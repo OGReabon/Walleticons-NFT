4677063de319a2849512943d858abc9a043d2279
Filip Faltejsek <filip.m.faltejsek@gmail.com>
2022-04-14 19:46:36 +0200

    Add function to generate bitMap from bytes

---
+++ services/createBitMap.ts
@@ -0,0 +1,32 @@
+const alignToByte = (byte) => {
+  let newByte = [];
+  // * There is no need to parse for 2 bytes as all characters from
+  // * ETH wallet addresses are just lowercase and uppercase letters
+  // * and numbers
+    while (byte.length < 8) {
+      byte.unshift('0')
+  }
+  byte.forEach((bit) => newByte.push(parseInt(bit,10)));
+  return newByte
+}
+
+export const parseIntobytes = (wallet) => {
+  let matrix = [];
+  let bytes = [];
+  wallet = wallet.split("")
+  wallet.forEach((char) => matrix.push(char.charCodeAt(0).toString(2).split(""))); // * generates byte code from wallet address
+  matrix.forEach((byte) => bytes.push(alignToByte(byte)))
+  return bytes;
+};
+
+export const generateBitMap = (bytes) => {
+// * This function takes bytes from ETH wallet address and
+// * generates an array with 2 bytes in each row
+    let bitMap = [];
+    // takes 2 bytes and pushes them into a single row in bitMap
+    for (let i = 0; i < bytes.length - 1; i += 2) {
+        let row = [...bytes[i], ...bytes[i + 1]]
+        bitMap.push(row)
+    }
+    return bitMap
+}
--- services/walleticonCreate.ts
+++ services/walleticonCreate.ts
@@ -1,27 +1,7 @@
 // TODO: parse req.body ✅
 // TODO: parse body into binary object ✅
 // TODO: create image from binary object
-
-const alignToByte = (byte) => {
-  let newByte = [];
-  // * There is no need to parse for 2 bytes as all characters from
-  // * ETH wallet addresses are just lowercase and uppercase letters
-  // * and numbers
-    while (byte.length < 8) {
-      byte.unshift('0')
-  }
-  byte.forEach((bit) => newByte.push(parseInt(bit,10)));
-  return newByte
-}
-
-export const parseIntoBits = (wallet) => {
-  let matrix = [];
-  let bits = [];
-  wallet = wallet.split("")
-  wallet.forEach((char) => matrix.push(char.charCodeAt(0).toString(2).split(""))); // * generates byte code from wallet address
-  matrix.forEach((byte) => bits.push(alignToByte(byte)))
-  return bits;
-};
+import { parseIntoBits } from './createBitMap'
 
 export const generateWalleticon = (walletBits) => {
   // pass
